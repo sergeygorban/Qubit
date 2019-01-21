e5cd6c0ef891396f2abde3331f19150fb9dd2475
СС <sergey.gorban.cc@gmail.com>
2019-01-21 12:27:19 +0200

    new

--- src/main/java/creating_object/StringToObject.java
+++ src/main/java/creating_object/StringToObject.java
@@ -2,6 +2,7 @@ package creating_object;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import http.Api;
 
 import java.util.Optional;
 
@@ -24,6 +25,20 @@ public class StringToObject {
         if (response != null && !response.isEmpty()) {
             try {
                 return new ObjectMapper().readerFor(cl).readValue(Optional.of(response).get());
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return null;
+    }
+
+    public <T> T createResponseObj(String response, Api api){
+
+        if (response != null && !response.isEmpty()) {
+            try {
+                return new ObjectMapper().readerFor(api.getClassForResponse()).readValue(Optional.of(response).get());
 
             } catch (Exception e) {
                 throw new RuntimeException(e);
