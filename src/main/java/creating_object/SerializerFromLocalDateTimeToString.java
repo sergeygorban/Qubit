c461ec2d863855f3d58326d29cb8eb1a1e39299b
СС <sergey.gorban.cc@gmail.com>
2019-03-20 14:14:54 +0200

    new

--- src/main/java/creating_object/SerializerFromLocalDateTimeToString.java
+++ src/main/java/creating_object/SerializerFromLocalDateTimeToString.java
@@ -5,16 +5,20 @@ import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 // It allows to serialize from LocalDateTime to String
 
 public class SerializerFromLocalDateTimeToString extends JsonSerializer<LocalDateTime> {
 
+    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+
     @Override
     public void serialize(LocalDateTime value, JsonGenerator generator, SerializerProvider provider){
 
         try {
-            generator.writeString(value.toString());
+            generator.writeString(value.format(formatter));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
