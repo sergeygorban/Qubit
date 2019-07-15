06896ff00ffb4fc528bf481253c81d323fba88bd
СС <sergey.gorban.cc@gmail.com>
2019-07-15 20:19:48 +0300

    new

--- src/main/java/creating_object/JsonValues.java
+++ src/main/java/creating_object/JsonValues.java
@@ -9,18 +9,43 @@ public class JsonValues {
 
 
     // Return json with changed value
-    public String changeValue(String parameter, String value, Object object) {
+    // Key: or "/fundingAccountInfo/encryptedPayload/encryptedData/TokenData" or "expiryMonth"
 
+    public String changeValue(String key, String value, Object object) {
+
+        JsonNode newNode = new ObjectMapper().convertValue(value, JsonNode.class);
         ObjectNode objectNode = new ObjectMapper().valueToTree(object);
-        objectNode.findParent(parameter).set(parameter, value == null ? null : new TextNode(value));
+
+        if (key.contains("/")) {
+
+            String parentParam = key.substring(0, key.lastIndexOf("/"));
+            String param = key.substring(key.lastIndexOf("/") + 1);
+            objectNode.at(parentParam).findParent(param).fields().forEachRemaining(p -> {
+
+                if (p.getKey().equals(param)) {
+                    p.setValue(value == null ? null : newNode);
+                }
+            });
+        } else {
+            objectNode.findParent(key).set(key, value == null ? null : newNode);        }
+
         return objectNode.toString();
     }
 
     // Return json with deleted parameter
-    public String removeParameter(String parameter, Object object) {
+    public String removeParameter(String key, Object object) {
 
         ObjectNode objectNode = new ObjectMapper().valueToTree(object);
-        objectNode.findParent(parameter).remove(parameter);
-        return objectNode.toString();
+
+        if (key.contains("/")) {
+
+            String parentParam = key.substring(0, key.lastIndexOf("/"));
+            String param = key.substring(key.lastIndexOf("/") + 1);
+            ObjectNode newObjectNode = (ObjectNode) objectNode.at(parentParam).findParent(param);
+            return newObjectNode.remove(param).toString();
+
+        } else {
+            return objectNode.findParent(key).remove(key).toString();
+        }
     }
 }
