216dcdbfb85d3d1f3c6a445801238a78513e7d01
СС <sergey.gorban.cc@gmail.com>
2020-01-28 14:22:49 +0200

    new

---
+++ src/main/java/time/DateTimeFormat.java
@@ -0,0 +1,19 @@
+package time;
+
+import lombok.Getter;
+
+@Getter
+public enum DateTimeFormat {
+
+    // 2020-05-14 00:00:00
+    ISO_1("yyyy-MM-dd HH:mm:ss"),
+    ISO_2("yyyy-MM-dd"),
+    ISO_3("yyyy-MM-dd 00:00:00");
+
+
+    private String format;
+
+    DateTimeFormat(String dateTimeFormat) {
+        this.format = dateTimeFormat;
+    }
+}
--- src/main/java/time/Time.java
+++ src/main/java/time/Time.java
@@ -7,6 +7,8 @@ import java.time.format.DateTimeFormatter;
 
 public class Time {
 
+
+
     public long getAmountDaysBetweenTwoDates(LocalDate firstDate, LocalDate secondDate) {
         return Duration.between(firstDate.atStartOfDay(), secondDate.atStartOfDay()).toDays();
     }
@@ -17,22 +19,6 @@ public class Time {
 
     public Duration getDurationBetweenTwoDates(LocalDateTime firstDate, LocalDateTime secondDate) {
         return Duration.between(firstDate, secondDate);
-    }
-
-
-    public static void main(String[] args) {
-
-        System.out.println(new Time().getAmountDaysForSqlServer(LocalDate.now()));
-
-        System.out.println(LocalDate.now().minusDays(5));
-
-        long a = new Time().getAmountDaysBetweenTwoDates(LocalDate.now(), LocalDate.parse("2019-10-30",
-                DateTimeFormatter.ofPattern("yyyy-MM-dd")));
-
-        System.out.println(a);
-        if (a >= 1) {
-            System.out.println(a);
-        }
     }
 
 }
