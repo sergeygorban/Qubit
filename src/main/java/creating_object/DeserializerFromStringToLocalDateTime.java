b18c71a97e9828f2f90d8668866161c406c0ec0c
СС <sergey.gorban.cc@gmail.com>
2020-07-02 13:26:15 +0300

    new

--- src/main/java/creating_object/DeserializerFromStringToLocalDateTime.java
+++ src/main/java/creating_object/DeserializerFromStringToLocalDateTime.java
@@ -3,6 +3,8 @@ package creating_object;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
+import time.DateTimeFormat;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -10,16 +12,19 @@ import java.time.format.DateTimeFormatter;
 
 public class DeserializerFromStringToLocalDateTime extends JsonDeserializer<LocalDateTime> {
 
-    private static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-
     @Override
     public LocalDateTime deserialize(JsonParser jsonparser, DeserializationContext context){
 
         try {
-            return LocalDateTime.parse(jsonparser.getText(), formatter);
+            return LocalDateTime.parse(jsonparser.getText(), DateTimeFormatter.ofPattern(DateTimeFormat.ISO_1.getFormat()));
 
         } catch (Exception e) {
-            throw new RuntimeException(e);
+
+            try {
+                return LocalDateTime.parse(jsonparser.getText(), DateTimeFormatter.ofPattern(DateTimeFormat.ISO_3.getFormat()));
+            } catch (Exception e1) {
+                throw new RuntimeException(e1);
+            }
         }
     }
 }
