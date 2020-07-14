6c089a6773013c5eeb529267c29da126a2d32c44
СС <sergey.gorban.cc@gmail.com>
2020-07-14 16:56:49 +0300

    new

--- src/main/java/time/DateTimeFormat.java
+++ src/main/java/time/DateTimeFormat.java
@@ -9,7 +9,8 @@ public enum DateTimeFormat {
     ISO_1("yyyy-MM-dd HH:mm:ss"),
     ISO_2("yyyy-MM-dd"),
     ISO_3("yyyy-MM-dd 00:00:00"),
-    ISO_4("1900-01-01 00:00:00");
+    ISO_4("1900-01-01 00:00:00"),
+    ISO_5("y-MM-dd 00:00:00");
 
 
     private String format;
